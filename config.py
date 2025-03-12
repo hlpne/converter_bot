6652fc1025a527f16b3b6e4d4fdfadefd4cb4e6e
hlpne <151643283+hlpne@users.noreply.github.com>
2025-03-12 10:31:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = "8076100692:AAH24G-Jb09S21kPE9QWe243bRRyWNI7Ipw"    # токен моего бота
+TOKEN = "-"    # токен моего бота
 EXCHANGE_API_URL = "https://api.exchangerate-api.com/v4/latest/"    # апишка с которой буду получать инфу по валюте
